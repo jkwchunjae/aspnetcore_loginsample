ec479651e3c42defd012b01c221ed23763fc5600
Kyungwon Jeon <jkwchunjae@daum.net>
2024-03-02 17:00:02 +0900

    sample

---
+++ BlazorApp_ExternalLoginSample/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using BlazorApp_ExternalLoginSample.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorApp_ExternalLoginSample.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
